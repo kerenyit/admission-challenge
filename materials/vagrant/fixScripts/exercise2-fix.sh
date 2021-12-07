bb5b9b0bf90178236d710b618e673787327a2cfd
kereny <kereny@traiana.com>
2021-12-07 19:56:26 +0200

    solution

--- materials/vagrant/fixScripts/exercise2-fix.sh
+++ materials/vagrant/fixScripts/exercise2-fix.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-#add fix to exercise2 here
+
+sed '/^127\.0\.0\.1[[:space:]]/d' /etc/hosts > /etc/hosts.new
+cat /etc/hosts.new > /etc/hosts
+rm /etc/hosts.new
--- materials/vagrant/fixScripts/exercise3-fix.sh
+++ materials/vagrant/fixScripts/exercise3-fix.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-#add fix to exercise3 here
+
+sed -i 's/Require all denied/Require all granted/g' /etc/apache2/sites-enabled/000-default.conf
+
+service apache2 restart
